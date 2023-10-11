c7c2e6cf751bda4e8272c5fa2de296ea0695bb07
Bryan Banz <bryan@5and2studio.com>
2023-10-10 21:10:09 -0500

    Add encryption keys to credentials

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,9 @@ module Ibcx
     #
     config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_record.encryption.primary_key         = Rails.application.credentials[Rails.env.to_sym][:active_record_encryption][:primary_key]
+    config.active_record.encryption.deterministic_key   = Rails.application.credentials[Rails.env.to_sym][:active_record_encryption][:deterministic_key]
+    config.active_record.encryption.key_derivation_salt = Rails.application.credentials[Rails.env.to_sym][:active_record_encryption][:key_derivation_salt]
   end
 end
